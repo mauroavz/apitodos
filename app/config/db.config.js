89020be60d29f20f359535f6ab84acdc5d127a2c
mauroavz <mauroramiro19@gmail.com>
2020-11-17 17:14:37 -0300

    database

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "tareas",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    HOST: "us-cdbr-iron-east-02.cleardb.net",
+    USER: "b7e2437887xxxa",
+    PASSWORD: "0200xxx6",
+    DB: "heroku_7643ec736354xxx",
   };
 
 /*
