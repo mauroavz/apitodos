6f5b9791f4fc3a664e128262854f4fca28f63760
mauroavz <mauroramiro19@gmail.com>
2020-11-23 10:25:30 -0300

    dfg

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://to-dos-tareas.herokuapp.com"
 };
 
 app.use(cors(corsOptions));
